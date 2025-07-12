d79f01acf200c728dc5e2544b72655610009e72c
RileyG7899 <rileyglance7899@gmail.com>
2025-07-12 15:11:36 -0700

    Finished bookbot

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+import sys
 from stats import count_words
 from stats import count_characters
+from stats import sort_list
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    print("============ BOOKBOT ============")
+    filepath = sys.argv[1]
+    file_contents = get_book_text(filepath)
+    print(f"Analyzing book found at {filepath}")
     word_count = count_words(file_contents)
+    print(f"Found {word_count} total words")
     character_counts = count_characters(file_contents)
     #print(file_contents)
-    print(f"{word_count} words found in the document")
-    print(character_counts)
-
+    #print(character_counts)
+    sorted_list = sort_list(character_counts)
+    for item in sorted_list:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 def count_words(file_contents):
     word_count = file_contents.split()
+    print("----------- Word Count ----------")
     return len(word_count)
 
 def count_characters(file_contents):
@@ -10,4 +11,18 @@ def count_characters(file_contents):
             character_counts[char] += 1
         else:
             character_counts[char] = 1
+    print("--------- Character Count -------")
     return character_counts
+
+# Returns num value
+def sort_on(dictionary):
+    return dictionary["num"]
+
+def sort_list(character_counts):
+    sorted_list = []
+    for char in character_counts:
+        if char != ' ':
+            new_dict = {"char": char, "num": character_counts[char]}
+            sorted_list.append(new_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return(sorted_list)
